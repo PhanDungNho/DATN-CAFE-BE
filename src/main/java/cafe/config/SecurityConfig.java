eb1e16ea1c0366855eab966f3e8ee1bc2e6338f5
PhanDungNho <dungnho.dev@gmail.com>
2024-10-16 10:34:59 +0700

    update sercurity

--- src/main/java/cafe/config/SecurityConfig.java
+++ src/main/java/cafe/config/SecurityConfig.java
@@ -40,7 +40,7 @@ public class SecurityConfig {
         return http.cors().and().csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(authorize -> authorize
                 		.requestMatchers("/api/v1/login", "/api/v1/account/image/**").permitAll()
-                		.requestMatchers("/api/v1/cart/**","/api/v1/categories/**","/api/v1/sizes/**","/api/v1/products/**","/api/v1/orders/**","/api/v1/account/**").hasAnyRole("ADMIN","STAFF")
+                		.requestMatchers("/api/v1/cart/**","/api/v1/categories/**","/api/v1/sizes/**","/api/v1/products/**","/api/v1/orders/**","/api/v1/account/**", "/api/v1/transactions/**").hasAnyRole("ADMIN","STAFF")
                 		.requestMatchers("/api/v1/admin/**").hasRole("ADMIN")
                         .requestMatchers("/home").permitAll()  
                         .requestMatchers("/cart/**","/order/**").hasAnyRole("ADMIN","STAFF")
