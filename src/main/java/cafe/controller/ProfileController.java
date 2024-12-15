c8ffefb3d7920ca08dd49fe473543147944be43a
buiminhtinh <minhtinh.dev@gmail.com>
2024-12-15 21:45:49 +0700

    Revert "update"
    This reverts commit 0955509d92d3ca820bcc190ba40d184af0108d19.

--- src/main/java/cafe/controller/ProfileController.java
+++ src/main/java/cafe/controller/ProfileController.java
@@ -13,7 +13,7 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/profile")
-@CrossOrigin(origins = "*")
+@CrossOrigin(origins = "http://localhost:3000")
 public class ProfileController {
 
     @Autowired
